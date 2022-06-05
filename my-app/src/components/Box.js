2f4285a08878f6735d5102fcd111a06c3e836dbd
Your Name <nj796644@gmail.com>
2022-06-05 20:31:16 +0530

    box component added

---
+++ my-app/src/components/Box.js
@@ -0,0 +1,26 @@
+export default function Box(props){
+return(
+    <div className = "box">
+        <img src ={props.img}/>
+        <div className = "box-right">
+            <div className = "box-location">
+                <img src = "./images/Fill 219.png" />
+                <span>{props.location}</span>
+                <a href = {props.googleMapsUrl} >View on google map</a>
+            </div>
+            <div className = "box-main">
+                <h1>{props.title}</h1>
+                <p>{props.startDate +"-"+props.endDate}</p>
+                <p>{props.detail}</p>
+            </div>
+        </div>
+    </div>
+)
+}
+/* title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        detail: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        img: "./images/Rectangle 77.png"*/
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return(
+        <nav>
+            <img src="./images/Fill 213.png" />
+            <h3>my travel journal</h3>
+                           
+        </nav>
+    )
+}
